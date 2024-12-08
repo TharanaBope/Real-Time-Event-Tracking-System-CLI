37ced71f5c297c3e4533d27b8f33d40aeb3ee1f3
Tharana Bopearachchi <tharanabope30@gmail.com>
2024-12-08 21:54:56 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Real-Time Ticketing System!");
+
+
+        System.out.println("System setup in progress...");
+
+    }
+}
